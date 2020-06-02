97c146074d2b4b163577c8a8363f39feb154f6c7
William Cao <wcao00@stuy.edu>
2020-06-02 14:16:45 -0400

    use session username for creator name

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from data import database_query
 from PIL import Image
 
@@ -6,12 +6,12 @@ api = Blueprint("api", __name__)
 
 @api.route("/image/create", methods=["POST"])
 def api_image_create():
-    print("hello")
+    if "username" not in session:
+        return "No permission", 403
     art_information = request.get_json()
     title = art_information["title"]
     image = art_information["image"]
-    # art_id = database_query.store_image(title, image, session["username"])
-    art_id = database_query.store_image(title, image, "mario")
+    art_id = database_query.store_image(title, image, session["username"])
     return {
         "id": art_id
     }
