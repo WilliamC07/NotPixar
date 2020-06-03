9ca97104da4c739be4ca2f8d3ef84f6ae968a8ff
William Cao <wcao00@stuy.edu>
2020-06-03 14:56:31 -0400

    can send images to frontend on home

--- app/__init__.py
+++ app/__init__.py
@@ -9,20 +9,7 @@ app.register_blueprint(api, url_prefix='/api')
 
 @app.route("/")
 def home():
-    images = [[
-        "https://vignette.wikia.nocookie.net/symbolism/images/4/43/Orange.png/revision/latest?cb=20140818120046",
-        "https://lh3.googleusercontent.com/proxy/8z88n9F4EFilgy7D9bRzF5dxCTbDL68w01SwL2VdYPohr7OxQUxdb16h3mG9vfaol7t4wgOaKeOQCSG2J2LLnqRV1Otb",
-        "https://webkit.org/blog-files/color-gamut/Webkit-logo-sRGB.png",
-        "https://www.iep.utm.edu/wp-content/media/blue.gif"
-    ], [
-        "https://colibriwp.com/blog/wp-content/uploads/2019/09/turquoise-1.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/21AoJYAb5lL._AC_SX425_.jpg",
-        "https://cdn-3d.niceshops.com/upload/image/product/large/default/vallejo-game-color-bald-moon-yellow-17-ml-279473-en.jpg"
-    ], [
-        "https://i1.wp.com/www.hisour.com/wp-content/uploads/2018/04/Blush-color.jpg?fit=720%2C720&ssl=1",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdhQCbLnGyjN8528xlVPSpirT6dB-dtOOiZ3hbSZIsfrcY6gEu&usqp=CAU",
-        "https://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg"
-    ]]
+    images = database_query.get_all_art()
     return render_template("home.html", images=images)
 
 @app.route("/create-account", methods=["POST", "GET"])
